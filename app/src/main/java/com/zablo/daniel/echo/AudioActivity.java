a5fcca911d60e79e5c8c125ae8b6ed21fad888ce
Zablodan <zablocki92@gmail.com>
2015-11-30 19:35:24 +0100

    Full Audio

--- app/src/main/java/com/zablo/daniel/echo/AudioActivity.java
+++ app/src/main/java/com/zablo/daniel/echo/AudioActivity.java
@@ -1,23 +1,59 @@
 package com.zablo.daniel.echo;
 
 
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Parcel;
+import android.os.UserHandle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Display;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-
+import java.util.List;
 
 
 public class AudioActivity extends AppCompatActivity {
@@ -25,8 +61,11 @@ public class AudioActivity extends AppCompatActivity {
     private String filePath = Environment.getExternalStorageDirectory().getPath();
     private File file = new File(filePath, "EchoSample");
     private ArrayList<String> FilesInFolder = GetFiles(filePath + "/EchoSample");
-
+    private Context actContext = this;
     private ExtAudioRecorder extAudioRecorder = null;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,33 +74,34 @@ public class AudioActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        if(!file.exists()){
+        if(!file.exists())
             file.mkdir();
-            Log.v("Create Folder", filePath);
-        }
 
+        ListView lv = (ListView)findViewById(R.id.filelist);
 
-        ListView lv;
-        lv = (ListView)findViewById(R.id.filelist);
-
+        if(FilesInFolder==null)
+            lv.setEmptyView(findViewById(R.id.emptyElement));
+        else
         lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, FilesInFolder));
+
+        registerForContextMenu(lv);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 Log.v("Item on lv clicked", "pos: " + position);
                 // Clicking on item
-            }
-        });
 
-        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
-                                           int position, long id) {
+                Uri uri = Uri.parse(file.getPath() + "/" + FilesInFolder.get(position));
+                MediaPlayer mPlayer = MediaPlayer.create(actContext, uri);
+                mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 
-                Log.v("long clicked", "pos: " + position);
-
-                return true;
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        // TODO Auto-generated method stub
+                        mp.release();
+                    }
+                });
+                mPlayer.start();
             }
         });
 
@@ -77,6 +117,39 @@ public class AudioActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.audiolist_menu, menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        ListView lv;
+        lv = (ListView)findViewById(R.id.filelist);
+        switch (item.getItemId()) {
+            case R.id.menuAnalyze:
+                //Analyze menu handle
+                return true;
+            case R.id.menuDelete:
+                //Delete menu handle
+                File filedel = new File(file.getPath() + "/" + FilesInFolder.get(item.getOrder()));
+                filedel.delete();
+                FilesInFolder = GetFiles(filePath + "/EchoSample");
+                if(FilesInFolder==null) {
+                    lv.setAdapter(null);
+                    lv.setEmptyView(findViewById(R.id.emptyElement));
+                }
+                else
+                lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, FilesInFolder));
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     public void onRecClick(View view) {
         Button RecBtn = (Button)findViewById(R.id.RecBtn);
@@ -96,6 +169,7 @@ public class AudioActivity extends AppCompatActivity {
         StopBtn.setClickable(false);
         RecBtn.setClickable(true);
         FilesInFolder = GetFiles(filePath + "/EchoSample");
+
         lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, FilesInFolder));
     }
 
@@ -117,7 +191,7 @@ public class AudioActivity extends AppCompatActivity {
         try{
             extAudioRecorder.prepare();
             extAudioRecorder.start();
-        }catch (Exception e){
+        }catch (Exception e) {
             e.printStackTrace();
         }
     }
